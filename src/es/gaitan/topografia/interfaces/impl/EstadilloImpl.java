2ad98cb8c1c6a8a4b8014816ff182d5f651ed98c
rafag <rafag@192.168.1.34>
2018-07-31 23:05:58 +0200

    Implementación de la interfaz Estadillo

---
+++ src/es/gaitan/topografia/interfaces/impl/EstadilloImpl.java
@@ -0,0 +1,13 @@
+package es.gaitan.topografia.interfaces.impl;
+
+import es.gaitan.topografia.interfaces.Estadillo;
+
+public class EstadilloImpl implements Estadillo {
+
+	@Override
+	public boolean anadirObservacion(String tipoObs, String linea) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
